72dafa7832a251a4dba73c407f7f6a7b91ceb661
unknown <huangyexin@BX-HUANGYEXIN.bxjr.com>
2016-10-28 10:25:27 +0800

    init

---
+++ src/components/title.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div>this is template body{{msg}}</div>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return{
+                msg:'hello vue组件'
+            }
+        }
+    }
+</script>
